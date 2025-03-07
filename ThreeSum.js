1feae6bc8363b173652222a9e6b279360c862a88
Bidhu1024 <bidhu1024av@gmail.com>
2025-03-07 22:16:46 +0530

    d

--- ThreeSum.js
+++ ThreeSum.js
@@ -1,19 +1,6 @@
 
 var threeSum = function(nums) {//error
-    let target =0;
-
-    if(nums.length < 3 ) return
-let returnArr = []
-    for(let i = 0;i<nums.length;i++){
-        for(let j = i+1;j<nums.length;j++){
-            for(let k = j+1;k<nums.length;k++){
-                if(nums[i]+nums[j]+nums[k] === target){
-                    returnArr.push([i,j,k])
-                }
-            }
-        }
-    }
-    return returnArr
+// trying two pointer approach
 };
 
 console.log(threeSum([-1,0,1,2,-1,-4]))
